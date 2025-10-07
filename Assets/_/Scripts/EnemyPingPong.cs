ae3a50d136b333c9c89ae4ced425e54f9740eb8d
Zhekach <62952369+Zhekach@users.noreply.github.com>
2025-10-07 18:29:02 +0300

    add simple enemy

---
+++ Assets/_/Scripts/EnemyPingPong.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using UnityEngine;
+
+[RequireComponent(typeof(Rigidbody2D), typeof(Collider2D))]
+public class EnemyPingPong : MonoBehaviour
+{
+    [SerializeField] private float _lengthX;
+    [SerializeField] private float _speed;
+
+    private Rigidbody2D _rigidbody;
+    private float _startX;  
+    
+    private void Start()
+    {
+        _rigidbody = GetComponent<Rigidbody2D>();
+        _startX = transform.position.x;
+        StartCoroutine(Move());
+    }
+
+    private IEnumerator Move()
+    {
+        while (enabled)
+        {
+            Vector3 newPosition = transform.position;
+            newPosition.x = _startX - Mathf.PingPong( _speed * Time.time, _lengthX);
+            _rigidbody.MovePosition(newPosition);
+            
+            yield return null;
+        }
+    }
+}
