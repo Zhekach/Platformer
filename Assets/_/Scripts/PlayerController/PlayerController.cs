bbc5b29e763e54ba6323b74b7c92d7165399d896
Zhekach <62952369+Zhekach@users.noreply.github.com>
2025-10-07 14:02:51 +0300

    add horizontal moving

--- Assets/_/Scripts/PlayerController/PlayerController.cs
+++ Assets/_/Scripts/PlayerController/PlayerController.cs
@@ -15,6 +15,8 @@ public class PlayerController : MonoBehaviour
 
     private void Update()
     {
+        _playerMovement.Move(_playerInput.HorizontalInput);
+        
         if(_playerInput.JumpInput)
             _playerMovement.Jump();
     }
--- Assets/_/Scripts/PlayerController/PlayerMovementSimple.cs
+++ Assets/_/Scripts/PlayerController/PlayerMovementSimple.cs
@@ -6,7 +6,8 @@ public class PlayerMovementSimple : MonoBehaviour, IPlayerMovement
 {
     [SerializeField] private GroundChecker _groundChecker;
     [SerializeField] private Rigidbody2D _rigidbody;
-    [SerializeField] private float _jumpForce = 5f;
+    [SerializeField] private float _jumpForce = 1f;
+    [SerializeField] private float _speed = 2f;
 
     private void Awake()
     {
@@ -16,7 +17,9 @@ public class PlayerMovementSimple : MonoBehaviour, IPlayerMovement
 
     public void Move(float horizontalSpeed)
     {
-        
+        Vector2 velocity = _rigidbody.linearVelocity;
+        velocity.x = _speed * horizontalSpeed;
+        _rigidbody.linearVelocity = velocity;
     }
 
     public void Jump()
