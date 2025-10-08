01b2f829a540b47fd546877c550f6418c7f0b9b7
Zhekach <62952369+Zhekach@users.noreply.github.com>
2025-10-08 22:48:19 +0300

    add coins

---
+++ Assets/_/Scripts/Coins/CoinsCollector.cs
@@ -0,0 +1,15 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(PlayerController), typeof(Collider2D))]
+public class CoinsCollector : MonoBehaviour
+{
+    [SerializeField] private int _count;
+    
+    private void OnTriggerEnter2D(Collider2D other)
+    {
+        if (other.gameObject.TryGetComponent<Coin>(out _))
+        {
+            _count++;
+        }
+    }
+}
---
+++ Assets/_/Scripts/Coins/CoinsSpawner.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class CoinsSpawner : MonoBehaviour
+{
+    [SerializeField] private List<Transform> _places;
+    [SerializeField] private Coin _coinPrefab;
+    
+    private void Start()
+    {
+        SpawnCoins();
+    }
+
+    private void SpawnCoins()
+    {
+        foreach (var place in _places)
+        {
+            Instantiate(_coinPrefab, place.position, place.rotation);
+            place.gameObject.SetActive(false);
+        }
+    }
+}
