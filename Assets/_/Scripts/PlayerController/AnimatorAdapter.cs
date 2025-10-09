e76c1532071ecf08c42fcb9b33915b37c93d72b2
Zhekach <62952369+Zhekach@users.noreply.github.com>
2025-10-09 14:54:24 +0300

    fix animatorAdapter
    Теперь дёргает анимации только при изменении параметров

--- Assets/_/Scripts/PlayerController/AnimatorAdapter.cs
+++ Assets/_/Scripts/PlayerController/AnimatorAdapter.cs
@@ -1,26 +1,55 @@
 ﻿using UnityEngine;
 
-[RequireComponent(typeof(Animator))]
+[RequireComponent(typeof(Animator), typeof(PlayerMovementSimple))]
 public class AnimatorAdapter : MonoBehaviour
 {
+    private const float SpeedDeltaMin = 0.01f;
+    
     [SerializeField] private Animator _animator;
-    [SerializeField] private PlayerMovementSimple _playerMovement;
+    [SerializeField] private IPlayerMovement _playerMovement;
     [SerializeField] private string _speedParamName = "SpeedAbs";
     [SerializeField] private string _isJumpingParamName = "IsJumping";
     
     private int _speedHash; 
-    private int _isJumpingHash; 
+    private int _isJumpingHash;
+
+    private float _lastSpeedAbs;
+    private bool _lastIsGrounded;
 
     private void Awake()
     {
         _animator = GetComponent<Animator>();
+        _playerMovement = GetComponent<IPlayerMovement>();
         _speedHash = Animator.StringToHash(_speedParamName);
         _isJumpingHash = Animator.StringToHash(_isJumpingParamName);
     }
 
     public void Update()
     {
-        _animator.SetFloat(_speedHash, Mathf.Abs(_playerMovement.Speed));
-        _animator.SetBool(_isJumpingHash, !_playerMovement.IsGrounded);
+        SetMovementAnimation(_playerMovement.Speed);
+        SetJumpAnimation(_playerMovement.IsGrounded);
+    }
+
+    private void SetMovementAnimation(float speed)
+    {
+        float speedAbs = Mathf.Abs(speed);
+        
+        if (speedAbs < SpeedDeltaMin)
+            speedAbs = 0f;
+        
+        if(Mathf.Approximately(speedAbs, _lastSpeedAbs))
+            return;
+        
+        _animator.SetFloat(_speedHash, speedAbs);
+        _lastSpeedAbs = speedAbs;
+    }
+
+    private void SetJumpAnimation(bool isGrounded)
+    {
+        if(isGrounded == _lastIsGrounded)
+            return;
+        
+        _animator.SetBool(_isJumpingHash, !isGrounded);
+        _lastIsGrounded = isGrounded;
     }
 }
