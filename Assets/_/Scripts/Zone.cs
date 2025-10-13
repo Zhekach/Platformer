81f038baa8dd4cfd63f1d1d548b3389fa2114ac4
Zhekach <62952369+Zhekach@users.noreply.github.com>
2025-10-13 16:29:38 +0300

    add zones spawner

---
+++ Assets/_/Scripts/Zone.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(BoxCollider2D))]
+public class Zone : MonoBehaviour
+{
+    [SerializeField] private BoxCollider2D _collider2D;
+    
+    private Vector2 _position;
+    
+
+    private void Awake()
+    {
+        _collider2D = GetComponent<BoxCollider2D>();
+        _position = transform.position;
+    }
+
+    public Vector2 GetRandomPosition()
+    {
+        Vector2 size = _collider2D.size;
+        Vector2 randomPoint = new Vector2(Random.Range(-size.x/2, size.x/2), Random.Range(-size.y/2, size.y/2));
+        Vector2 randomPosition = _position + randomPoint;
+        
+        return randomPosition;
+    }
+    
+    private void OnValidate()
+    {
+        _position = transform.position;
+    }
+}
---
+++ Assets/_/Scripts/ZonesSpawner.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class ZonesSpawner : MonoBehaviour
+{
+    [SerializeField] private List<Zone> _zones;
+    [SerializeField] private GameObject _prefab;
+    [SerializeField] private int _spawnsCount;
+    
+    
+    private void Start()
+    {
+        for(var i = 0; i < _spawnsCount; i++)
+        {
+            var zone = _zones[Random.Range(0, _zones.Count)];
+            var position = zone.GetRandomPosition();
+            var rotation = zone.transform.rotation;
+            
+            var instance = Instantiate(_prefab, position, rotation);
+            instance.transform.parent = zone.transform;
+        }
+    }
+}
