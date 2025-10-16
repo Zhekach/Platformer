561350452ed724dd743ec717ecb7932b6c08ca7a
Zhekach <62952369+Zhekach@users.noreply.github.com>
2025-10-16 16:27:20 +0300

    add vampirism indicator

--- Assets/_/Scripts/Vampirism/VampirismBarIndicator.cs
+++ Assets/_/Scripts/Vampirism/VampirismBarIndicator.cs
@@ -16,4 +16,37 @@ public class VampirismBarIndicator : MonoBehaviour
     {
         _timer = timer;
     }
+    
+    private void Update()
+    {
+        switch (_timer.State)
+        {
+            case(VampirismState.Ready):
+                SetReadyIndication();        
+                break;
+            case(VampirismState.Cooldown):
+                SetCooldownIndication();
+                break;
+            case(VampirismState.Active):
+                SetActiveIndication();
+                break;
+        }
+            
+        _slider.fillRect.gameObject.SetActive(_slider.value != 0);
+    }
+
+    private void SetActiveIndication()
+    {
+        _slider.value = _timer.ActiveTimeLeft / _timer.ActiveTimeMax * _slider.maxValue;
+    }
+
+    private void SetCooldownIndication()
+    {
+        _slider.value = _slider.maxValue - _timer.CooldownTimeLeft / _timer.CooldownTimeMax * _slider.maxValue;
+    }
+
+    private void SetReadyIndication()
+    {
+        _slider.value = _slider.maxValue;
+    }
 }
--- Assets/_/Scripts/Vampirism/VampirismTimer.cs
+++ Assets/_/Scripts/Vampirism/VampirismTimer.cs
@@ -1,44 +1,75 @@
-﻿using System.Collections;
+﻿using System;
+using System.Collections;
 using UnityEngine;
 
 public class VampirismTimer : MonoBehaviour
 {
-    [SerializeField] private float _activeMaxTime = 6f;
-    [SerializeField] private float _cooldownMaxTime = 4f;
+    [SerializeField] private float _activeTimeMax = 6f;
+    [SerializeField] private float _cooldownTimeMax = 4f;
     [SerializeField] private VampirismState _state;
 
     private Coroutine _coroutine;
-    
+    private WaitForSeconds _activeWait;
+    private WaitForSeconds _cooldownWait;
+    private float _activeTimeLeft;
+    private float _cooldownTimeLeft;
+
     public VampirismState State => _state;
+    public float ActiveTimeMax => _activeTimeMax;
+    public float ActiveTimeLeft => _activeTimeLeft;
+    public float CooldownTimeMax => _cooldownTimeMax;
+    public float CooldownTimeLeft => _cooldownTimeLeft;
 
     private void Start()
     {
         _state = VampirismState.Ready;
+        _activeWait = new WaitForSeconds(_activeTimeMax);
+        _cooldownWait = new WaitForSeconds(_cooldownTimeMax);
     }
-    
+
+    private void Update()
+    {
+        if (_coroutine != null)
+            return;
+
+        if (_state == VampirismState.Cooldown )
+        {
+            _cooldownTimeLeft = _cooldownTimeMax;
+            _coroutine = StartCoroutine(CooldownTimer());
+        }
+    }
+
     public void Activate()
     {
-        if(_state != VampirismState.Ready)
+        if (_state != VampirismState.Ready)
             return;
-        
+
         _state = VampirismState.Active;
+        _activeTimeLeft = _activeTimeMax;
         _coroutine = StartCoroutine(ActiveTimer());
     }
-    
+
     private IEnumerator ActiveTimer()
     {
-        yield return new WaitForSeconds(_activeMaxTime);
+        while (_activeTimeLeft > 0)
+        {
+            _activeTimeLeft -= Time.deltaTime;
+            yield return null;
+        }
+
         _state = VampirismState.Cooldown;
-        
         _coroutine = null;
-        _coroutine = StartCoroutine(CooldownTimer());
     }
-    
+
     private IEnumerator CooldownTimer()
     {
-        yield return new WaitForSeconds(_cooldownMaxTime);
+        while (_cooldownTimeLeft > 0)
+        {
+            _cooldownTimeLeft -= Time.deltaTime;
+            yield return null;
+        }
+
         _state = VampirismState.Ready;
-        
         _coroutine = null;
     }
 }
