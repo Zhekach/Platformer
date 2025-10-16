8517149653bed9570d0ab19e6ad97c2374d653ea
Zhekach <62952369+Zhekach@users.noreply.github.com>
2025-10-16 15:10:27 +0300

    add view

--- Assets/_/Scripts/Vampirism/Vampirism.cs
+++ Assets/_/Scripts/Vampirism/Vampirism.cs
@@ -1,20 +1,24 @@
 ﻿using System;
 using UnityEngine;
 
-[RequireComponent(typeof(VampirismDetector), typeof(VampirismTimer))]
+[RequireComponent(typeof(VampirismDetector), typeof(VampirismTimer), typeof(VampirismView))]
 public class Vampirism : MonoBehaviour
 {
     [SerializeField] private float _healPerSecond;
     [SerializeField] private float _radius;
     [SerializeField] private VampirismDetector _detector;
     [SerializeField] private VampirismTimer _timer;
+    [SerializeField] private VampirismView _view;
     [SerializeField] private Health _health;
     
     private void Awake()
     {
         _detector = GetComponent<VampirismDetector>();
         _timer = GetComponent<VampirismTimer>();
+        _view = GetComponent<VampirismView>();
+
         _detector.Initialize(_radius);
+        _view.Initialize(_timer, _radius);
     }
 
     private void FixedUpdate()
---
+++ Assets/_/Scripts/Vampirism/VampirismView.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using UnityEngine;
+
+[RequireComponent(typeof(SpriteRenderer))]
+public class VampirismView : MonoBehaviour
+{
+    [SerializeField] private SpriteRenderer _spriteRenderer;
+    [SerializeField] private Color _activeColor;
+    [SerializeField] private Color _cooldownColor;
+    [SerializeField] private Color _readyColor;
+
+    private VampirismTimer _timer;
+    private float _radius;
+
+    private void Awake()
+    {
+        if(_spriteRenderer == null)
+            throw new ArgumentNullException($"Not assigned {nameof(SpriteRenderer)} to {name}");
+    }
+
+    private void Update()
+    {
+        SetState();
+    }
+
+    public void Initialize(VampirismTimer timer, float radius)
+    {
+        _timer = timer;
+        _radius = radius;
+        _spriteRenderer.transform.localScale = new Vector3(_radius * 2f, _radius * 2f, 1f);
+    }
+
+    private void SetState()
+    {
+        VampirismState state = _timer.State;
+        
+        switch (state)
+        {
+            case VampirismState.Ready:
+                _spriteRenderer.color = _readyColor;
+                break;
+            case VampirismState.Cooldown:
+                _spriteRenderer.color = _cooldownColor;
+                break;
+            case VampirismState.Active:
+                _spriteRenderer.color = _activeColor;
+                break;
+        }
+    }
+}
